def1d2c8bd8d419ffa9bd919ab04f12772cef964
folong201 <folong201@gmail.com>
2025-06-30 17:58:34 +0100

    Refactor App component: update title and remove unused code

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,17 @@
-import { useState } from 'react'
+// import { useState } from 'react'
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1>Company website</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Random website</h1>
+      
       <div>
-        <center>The website</center>
+        <center>The website from backend</center>
       </div>
       <div style={{ marginTop: '2rem' }}>
         <iframe
